d9351517e67932b701d5545e2b902a24358c4684
Charlotte <Charlotte@172.16.123.22>
2021-03-31 11:12:16 +0200

    add Transformateur3MarqueChocolat

---
+++ src/abstraction/eq5Transformateur3/Trandformateur3MarqueChocolat.java
@@ -0,0 +1,15 @@
+package abstraction.eq5Transformateur3;
+
+import java.util.List;
+
+import abstraction.fourni.IMarqueChocolat;
+
+public class Trandformateur3MarqueChocolat implements IMarqueChocolat {
+
+	@Override
+	public List<String> getMarquesChocolat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
