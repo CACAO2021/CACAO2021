40988c04ea2894fbfecd3c36d82346370ff8e743
lebra <lebra@172.16.128.198>
2021-03-31 11:54:18 +0200

    Fonction peutVendre

--- src/abstraction/eq1Producteur1/VendeurContratCadre1.java
+++ src/abstraction/eq1Producteur1/VendeurContratCadre1.java
@@ -6,6 +6,9 @@ import java.util.List;
 import abstraction.eq8Romu.contratsCadres.Echeancier;
 import abstraction.eq8Romu.contratsCadres.ExemplaireContratCadre;
 import abstraction.eq8Romu.contratsCadres.IVendeurContratCadre;
+import abstraction.eq8Romu.produits.Chocolat;
+import abstraction.eq8Romu.produits.Feve;
+import abstraction.eq8Romu.produits.Gamme;
 import abstraction.fourni.Filiere;
 import abstraction.fourni.IActeur;
 import abstraction.fourni.Journal;
@@ -13,10 +16,27 @@ import abstraction.fourni.Variable;
 
 public class VendeurContratCadre1 extends Producteur1Acteur implements IVendeurContratCadre{
 
-	@Override
+	/**
+	 * @author lebra
+	 */
 	public boolean peutVendre(Object produit) {
-		// TODO Auto-generated method stub
-		return false;
+		if ((produit instanceof Feve)
+				&& ((((Feve)produit) == Feve.FEVE_MOYENNE_EQUITABLE)
+						||(((Feve)produit) == Feve.FEVE_MOYENNE)
+						||(((Feve)produit) == Feve.FEVE_BASSE)
+					)
+			) {
+				return(true);
+		}
+		else if ((produit instanceof Chocolat)
+				&& ((((Chocolat)produit) == Chocolat.POUDRE_MOYENNE_EQUITABLE)
+					||(((Chocolat)produit) == Chocolat.POUDRE_MOYENNE))) {
+			return (true); 
+		}
+		else {
+			return (false);
+		}
+		
 	}
 
 	@Override
