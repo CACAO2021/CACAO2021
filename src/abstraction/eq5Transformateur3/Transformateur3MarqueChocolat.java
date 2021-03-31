d66b9393c798f2bf028e8dcc37421223d2ab37a1
lena- <lena-@172.16.123.184>
2021-03-31 11:57:31 +0200

    Implementation IMarqueChocolat

--- src/abstraction/eq5Transformateur3/Transformateur3MarqueChocolat.java
+++ src/abstraction/eq5Transformateur3/Transformateur3MarqueChocolat.java
@@ -1,15 +1,25 @@
 package abstraction.eq5Transformateur3;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import abstraction.fourni.IMarqueChocolat;
 
 public class Transformateur3MarqueChocolat implements IMarqueChocolat {
+	private String marque;
 
-	@Override
+	public Transformateur3MarqueChocolat() {
+		this.marque = "Côte d'IMT";
+	}
+	
+	public String getMarque() {
+		return this.marque;
+	}
+	
 	public List<String> getMarquesChocolat() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> marques = new ArrayList<String>();
+		marques.add(getMarque());
+		return marques;
 	}
 
 }
