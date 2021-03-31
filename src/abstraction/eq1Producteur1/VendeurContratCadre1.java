a15ac6e5c91b644b51df764661de29290ed1af4e
arthurlemerle <arthurlemerle@dhcp-127-237.mde.emn.fr>
2021-03-31 11:21:21 +0200

    ajout des variables d'instance de la classe VendeurContratCadre1 comme dans la classe exemple

--- src/abstraction/eq1Producteur1/VendeurContratCadre1.java
+++ src/abstraction/eq1Producteur1/VendeurContratCadre1.java
@@ -6,12 +6,20 @@ import java.util.List;
 import abstraction.eq8Romu.contratsCadres.Echeancier;
 import abstraction.eq8Romu.contratsCadres.ExemplaireContratCadre;
 import abstraction.eq8Romu.contratsCadres.IVendeurContratCadre;
+import abstraction.eq8Romu.contratsCadres.SuperviseurVentesContratCadre;
 import abstraction.fourni.Filiere;
 import abstraction.fourni.IActeur;
 import abstraction.fourni.Journal;
 import abstraction.fourni.Variable;
 
 public class VendeurContratCadre1 extends Producteur1Acteur implements IVendeurContratCadre{
+	private static int NB_INSTANCES = 0; // Afin d'attribuer un nom different a toutes les instances
+	protected int numero;
+	protected Variable stock;
+	protected Integer cryptogramme;
+	protected Object produit;
+	protected Journal journal;
+	protected SuperviseurVentesContratCadre supCCadre;
 
 	@Override
 	public boolean peutVendre(Object produit) {
