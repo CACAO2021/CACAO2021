cb8fa8f753c2167cc0c26aba4727d86e80abbfbb
paulgiraud <paulgiraud@dhcp-125-142.mde.emn.fr>
2021-03-31 11:33:04 +0200

    class frabricant

--- src/abstraction/eq3Transformateur1/Transformateur1FabricantChocolatDeMarque.java
+++ src/abstraction/eq3Transformateur1/Transformateur1FabricantChocolatDeMarque.java
@@ -1,5 +1,99 @@
 package abstraction.eq3Transformateur1;
 
-public class Transformateur1FabricantChocolatDeMarque {
+import java.awt.Color;
+import java.util.List;
+
+import abstraction.eq8Romu.produits.ChocolatDeMarque;
+import abstraction.fourni.Filiere;
+import abstraction.fourni.IActeur;
+import abstraction.fourni.IFabricantChocolatDeMarque;
+import abstraction.fourni.Journal;
+import abstraction.fourni.Variable;
+
+public class Transformateur1FabricantChocolatDeMarque implements IFabricantChocolatDeMarque {
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initialiser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<String> getNomsFilieresProposees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Filiere getFiliere(String nom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Variable> getIndicateurs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Variable> getParametres() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Journal> getJournaux() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCryptogramme(Integer crypto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notificationFaillite(IActeur acteur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notificationOperationBancaire(double montant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ChocolatDeMarque> getChocolatsProduits() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
