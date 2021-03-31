adaae24e02454192cddbff0d8244765e10b84c8c
Dimitri CORREIA <correiadimitri@sfr.fr>
2021-03-31 10:28:11 +0200

    remise a zero

--- src/abstraction/eq2Producteur2/Producteur2Acteur.java
+++ src/abstraction/eq2Producteur2/Producteur2Acteur.java
@@ -4,20 +4,15 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
-import abstraction.eq8Romu.contratsCadres.Echeancier;
-import abstraction.eq8Romu.contratsCadres.ExemplaireContratCadre;
-import abstraction.eq8Romu.contratsCadres.IVendeurContratCadre;
 import abstraction.fourni.Filiere;
 import abstraction.fourni.IActeur;
 import abstraction.fourni.Journal;
 import abstraction.fourni.Variable;
 
-public class Producteur2Acteur implements IActeur, IVendeurContratCadre {
+public class Producteur2Acteur implements IActeur {
 	protected int cryptogramme;
-	protected Variable stockFeves;
 
 	public Producteur2Acteur() {
-		this.stockFeves=new Variable(getNom()+" stock feves", this, 0, 100000, 10000);
 	}
 
 	public void initialiser() {
@@ -82,49 +77,5 @@ public class Producteur2Acteur implements IActeur, IVendeurContratCadre {
 	public double getSolde() {
 		return Filiere.LA_FILIERE.getBanque().getSolde(this, this.cryptogramme);
 	}
-
-	@Override
-	public boolean peutVendre(Object produit) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean vend(Object produit) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Echeancier contrePropositionDuVendeur(ExemplaireContratCadre contrat) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public double propositionPrix(ExemplaireContratCadre contrat) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double contrePropositionPrixVendeur(ExemplaireContratCadre contrat) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void notificationNouveauContratCadre(ExemplaireContratCadre contrat) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public double livrer(Object produit, double quantite, ExemplaireContratCadre contrat) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-	//
 
 }
