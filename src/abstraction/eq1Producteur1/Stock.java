0bac3fd6b8792141f722fdc2e4ba352098d8c491
Alb1x <alban.tavolacci@imt-atlantique.net>
2021-04-03 18:08:44 +0200

    Ajout d'une description et quelques méthodes.

--- src/abstraction/eq1Producteur1/Stock.java
+++ src/abstraction/eq1Producteur1/Stock.java
@@ -1,17 +1,31 @@
 package abstraction.eq1Producteur1;
 
+/**
+ * @author arthurlemgit
+ * @author Alb1x
+ * Implémentation basique du stock pour le moment (juste un nombre de fèves), quantité en kg.
+ */
 public class Stock {
 	private double quantite;
 	public Stock(double quantite) {
 		this.quantite=quantite;
 	}
-	private double getQuantite() {
+	public double getQuantite() {
 		return quantite;
 	}
 	
 	private void setQuantite(double d) {
 		this.quantite=d;
 	}
+	
+	public void addQuantite(double d) {
+		this.quantite+=d;
+	}
+	
+	public void removeQuantite(double d) {
+		this.quantite-=d;
+	}
+	
 	
 	
 }
