73fb81edb303436d72002f6ea2d2d5b7256d57a9
Elio <Elio@DESKTOP-6JM2O2H>
2021-04-06 14:57:12 +0200

    Ajout d'une Classe Abstract pour définir les caractéristiques générales de l'acteur

---
+++ src/abstraction/eq7Distributeur2/AbsDistributeur2.java
@@ -0,0 +1,34 @@
+package abstraction.eq7Distributeur2;
+
+import java.util.ArrayList;
+
+import abstraction.eq8Romu.produits.Chocolat;
+
+public abstract class AbsDistributeur2 {
+	
+	public ArrayList<Chocolat> chocolatPropose;
+
+	public AbsDistributeur2() {
+		chocolatPropose = new ArrayList<Chocolat>();
+		chocolatPropose.add(Chocolat.CONFISERIE_HAUTE_BIO_EQUITABLE);
+		chocolatPropose.add(Chocolat.CONFISERIE_HAUTE_EQUITABLE);
+		chocolatPropose.add(Chocolat.CONFISERIE_MOYENNE_EQUITABLE);
+		chocolatPropose.add(Chocolat.CONFISERIE_MOYENNE);
+		chocolatPropose.add(Chocolat.TABLETTE_HAUTE_BIO_EQUITABLE);
+		chocolatPropose.add(Chocolat.TABLETTE_HAUTE_EQUITABLE);
+		chocolatPropose.add(Chocolat.TABLETTE_MOYENNE_EQUITABLE);
+		chocolatPropose.add(Chocolat.TABLETTE_MOYENNE);
+		chocolatPropose.add(Chocolat.POUDRE_HAUTE_BIO_EQUITABLE);
+		chocolatPropose.add(Chocolat.POUDRE_HAUTE_EQUITABLE);
+		chocolatPropose.add(Chocolat.POUDRE_MOYENNE_EQUITABLE);
+		chocolatPropose.add(Chocolat.POUDRE_MOYENNE);
+		
+		
+
+		// Liste des Chocolats que nous proposons aux clients finaux
+		
+		
+		
+	}
+
+}
