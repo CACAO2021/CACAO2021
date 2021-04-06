9398f35563b2240b1d16c8d675cae91dfc417800
paulgiraud <paulgiraud@localhost>
2021-04-06 22:00:57 +0200

    creation de la classe stock, elle aura pour but d'avoir l'information sur le stock de chaque produit/ressource

---
+++ src/abstraction/eq3Transformateur1/Stock.java
@@ -0,0 +1,19 @@
+package abstraction.eq3Transformateur1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import abstraction.eq8Romu.produits.Feve;
+import abstraction.fourni.Variable;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Stock {
+	
+	private Transformateur1Acteur eticao;
+	private List<Variable> indicteurs;
+	protected Map<Feve, Double> stocksFeves;
+	
+
+}
