d70b95454c5bc35217076ad28377b3cdd115bae8
Maxime <Maxime@LAPTOP-SO8TAPIU>
2021-04-07 12:11:15 +0200

    Fin de séance

--- src/abstraction/eq2Producteur2/Producteur2VendeurFeveAO.java
+++ src/abstraction/eq2Producteur2/Producteur2VendeurFeveAO.java
@@ -2,8 +2,10 @@ package abstraction.eq2Producteur2;
 
 
 import java.util.LinkedList;
+import java.util.Map;
 
 import abstraction.eq8Romu.contratsCadres.ExemplaireContratCadre;
+import abstraction.eq8Romu.fevesAO.IAcheteurFevesAO;
 import abstraction.eq8Romu.fevesAO.IVendeurFevesAO;
 import abstraction.eq8Romu.fevesAO.OffreAchatFeves;
 import abstraction.eq8Romu.fevesAO.PropositionVenteFevesAO;
@@ -14,6 +16,7 @@ public class Producteur2VendeurFeveAO extends Producteur2Transfo implements IVen
 	protected LinkedList<ExemplaireContratCadre> mesContratsAO;
 	public LinkedList<PropositionVenteFevesAO> mesContratsAORefuses;
 	public LinkedList<PropositionVenteFevesAO> mesContratsAOAcceptes;
+	//nbOffres=nbOffres.SuperviseurVentesFevesAO;
 
 	public Producteur2VendeurFeveAO() {
 		super();
@@ -25,27 +28,28 @@ public class Producteur2VendeurFeveAO extends Producteur2Transfo implements IVen
 	 */
 	public double proposerPrix(OffreAchatFeves oa) {
 	Feve feveachetee=oa.getFeve();
+	int nbOffres=nbOffres.get(acheteurs.get(acheteurCourant);
 	
-	if ((feveachetee==Feve.FEVE_HAUTE_BIO_EQUITABLE)&&(stockFeveHBE.qttTotale())) {
+	if ((feveachetee==Feve.FEVE_HAUTE_BIO_EQUITABLE)&&(qttTotale(Feve.FEVE_HAUTE_BIO_EQUITABLE)>oa.getQuantiteKG())) {
 		return PRIX_ESPERE_FEVE_HBE;
 	}
-	else if (feveachetee==Feve.FEVE_HAUTE_EQUITABLE) {
+	else if ((feveachetee==Feve.FEVE_HAUTE_EQUITABLE)&&(qttTotale(Feve.FEVE_HAUTE_EQUITABLE)>oa.getQuantiteKG())) {
 		return PRIX_ESPERE_FEVE_HE;
 	}
-	else if (feveachetee==Feve.FEVE_MOYENNE_EQUITABLE) {
+	else if ((feveachetee==Feve.FEVE_MOYENNE_EQUITABLE)&&(qttTotale(Feve.FEVE_MOYENNE_EQUITABLE)>oa.getQuantiteKG())) {
 		return PRIX_ESPERE_FEVE_ME;
 	}
-	else if (feveachetee==Feve.FEVE_MOYENNE) {
+	else if ((feveachetee==Feve.FEVE_MOYENNE)&&(qttTotale(Feve.FEVE_MOYENNE)>oa.getQuantiteKG())) {
 		return PRIX_ESPERE_FEVE_M;
 	}
-	else if (feveachetee==Feve.FEVE_BASSE) {
+	else if ((feveachetee==Feve.FEVE_BASSE)&&(qttTotale(Feve.FEVE_BASSE)>oa.getQuantiteKG())) {
 		return PRIX_ESPERE_FEVE_B;
 	}
-	
 	else {
 		return 0.0;
 	}
 	}
+	
 /**	@author Maxime Boillot
 	 
 	 
