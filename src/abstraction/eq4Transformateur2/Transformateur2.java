aa43e39eba38afe077a9a9d47efcee2a94c9b6ca
pierretteramires <pierretteramires@dhcp-127-20.mde.emn.fr>
2021-04-07 12:03:23 +0200

    test

--- src/abstraction/eq4Transformateur2/Transformateur2.java
+++ src/abstraction/eq4Transformateur2/Transformateur2.java
@@ -5,5 +5,11 @@ public class Transformateur2 extends Transformateur2Acteur {
 	public Transformateur2 () {
 		super();
 	}
+
+	@Override
+	public double get_stock(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 }
--- src/abstraction/eq4Transformateur2/Transformateur2Production.java
+++ src/abstraction/eq4Transformateur2/Transformateur2Production.java
@@ -1,6 +1,12 @@
 package abstraction.eq4Transformateur2;
 
 public class Transformateur2Production extends Transformateur2Acteur {
+
+	@Override
+	public double get_stock(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 	
 }
