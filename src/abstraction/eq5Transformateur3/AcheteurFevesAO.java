b56214e0557f506a582e925ddbe8885a7550a14f
Charlotte <Charlotte@172.16.123.22>
2021-04-07 12:11:52 +0200

    AO

--- src/abstraction/eq5Transformateur3/AcheteurFevesAO.java
+++ src/abstraction/eq5Transformateur3/AcheteurFevesAO.java
@@ -1,95 +1,72 @@
 package abstraction.eq5Transformateur3;
-
+//Charlotte
 import java.awt.Color;
 import java.util.List;
 
-import abstraction.eq8Romu.fevesAO.ExempleAbsAcheteurFevesAO;
 import abstraction.eq8Romu.fevesAO.IAcheteurFevesAO;
 import abstraction.eq8Romu.fevesAO.OffreAchatFeves;
 import abstraction.eq8Romu.fevesAO.PropositionVenteFevesAO;
 import abstraction.eq8Romu.fevesAO.SuperviseurVentesFevesAO;
 import abstraction.eq8Romu.produits.Feve;
-import abstraction.fourni.Filiere;
-import abstraction.fourni.IActeur;
-import abstraction.fourni.Journal;
 import abstraction.fourni.Variable;
+import abstraction.eq8Romu.produits.Chocolat;
 
-	public class  AcheteurFevesAO extends ExempleAbsAcheteurFevesAO implements IAcheteurFevesAO {
+public class  AcheteurFevesAO extends Transformateur3Stock implements IAcheteurFevesAO {
+	private Variable quantite;
+	private Feve feve;
+	private double qmin;
+	private double qmax;
+	
+	public AcheteurFevesAO() {
+		quantite = new Variable("quantite", this, qmin,qmax,0); //qmin et qmax représentent les quantites de chocolat (et pas de fèves!!)
+		this.feve = feve;
+	}
+	public double getQmin() {
+		return this.quantite.getMin();
+	}
+	public double getQmax() {
+		return this.quantite.getMax();
+	}
 
-		private Feve feve;
-		private double prixMax;
-		private double qMin, qMax;
-
-		public AcheteurFevesAO(Feve feve, double prixMax, double qMin, double qMax) {
-			this.feve = feve;
-			this.prixMax = prixMax;
-			this.qMin = qMin;
-			this.qMax = qMax;
-		}
-
-		public Integer getCryptogramme(SuperviseurVentesFevesAO superviseur) {
-			if (superviseur!=null) { // Personne ne peut creer un second Superviseur --> il s'agit bien de l'unique superviseur et on peut lui faire confiance
-				return cryptogramme;
-			}
-			return Integer.valueOf(0);
-		}
-
-		public OffreAchatFeves getOffreAchat() {
-			OffreAchatFeves result = new OffreAchatFeves(this, feve, qMin+(Math.random()*(qMax-qMin)));
-			if (Math.random()<0.5) { // une chance sur 2
-				this.journal.ajouter("Offre d'achat = "+result);
-				return result;
-			} else {
-				this.journal.ajouter("pas d'offre d'achat");
-				return null;
-			}
-		}
-
-		public void notifierAucuneProposition(OffreAchatFeves oa) {
-			this.journal.ajouter("--> aucune proposition de vente pour l'offre "+oa);
-		}
-
-		public PropositionVenteFevesAO choisirPropositionVenteAOFeves(List<PropositionVenteFevesAO> propositions) {
-			if (propositions.size()>0) {
-				int hasard = (int)(Math.random()*propositions.size());
-				if (propositions.get(hasard).getPrixKG()<this.prixMax) {
-					return propositions.get(hasard);
+	@Override
+	public OffreAchatFeves getOffreAchat() {
+		OffreAchatFeves OA = new OffreAchatFeves(this, feve, quantite.getValeur());
+			for(Chocolat chocolat : this.getChocolats().keySet()) {
+				if(this.getChocolats().get(chocolat).getValeur() < this.getQmin()) { 
+					quantite.ajouter(this, (this.getQmax()-this.getQmin())*25);//40 g de feves pour 100 g de chocolat (la quantité represente la quantite de fèves il faut donc convertir pour pouvoir comparer la )
 				}
 			}
-			return null;
-		}
+		
+		
+		return null;
+	}
 
-		public void notifierVente(PropositionVenteFevesAO proposition) {
-			stocksFeves.put(feve, stocksFeves.get(feve)+proposition.getQuantiteKg());
-			this.journal.ajouter("--> le stock de feve passe a "+Journal.doubleSur(this.stocksFeves.get(proposition.getFeve()), 4));
-		}
+	@Override
+	public void notifierAucuneProposition(OffreAchatFeves oa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PropositionVenteFevesAO choisirPropositionVenteAOFeves(List<PropositionVenteFevesAO> propositions) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getCryptogramme(SuperviseurVentesFevesAO superviseur) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void notifierVente(PropositionVenteFevesAO proposition) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
 
 		
-		}
 
-		//	public double proposerAchat(LotCacaoCriee lot) {
-		//		double prix = this.prixCourant.get(lot.getFeve())*0.95; // on tente de payer un peu moins
-		//		double solde = Filiere.LA_FILIERE.getBanque().getSolde(this, cryptogramme);
-		//		if (solde>lot.getQuantiteEnTonnes()*prix) {
-		//			this.journal.ajouter("Propose "+Journal.doubleSur(prix,  4)+" pour "+Journal.texteColore(lot.getVendeur(), Journal.doubleSur(lot.getQuantiteEnTonnes(), 2)+" tonnes de "+lot.getFeve().name()));
-		//			return prix;
-		//		} else {
-		//			this.journal.ajouter("Ne souhaite pas acheter un lot de "+Journal.texteColore(lot.getVendeur(), Journal.doubleSur(lot.getQuantiteEnTonnes(), 2)+" tonnes de "+lot.getFeve().name()));
-		//			return 0.0;
-		//		}
-		//	}
-		//
-		//	public void notifierPropositionRefusee(PropositionCriee proposition) {
-		//		double nouveauPrix = Math.max(0.01,  this.prixCourant.get(proposition.getLot().getFeve())*1.02);
-		//		this.prixCourant.put(proposition.getLot().getFeve(), nouveauPrix);
-		//		this.journal.ajouter("Apprend que sa proposition de "+Journal.doubleSur(proposition.getPrixPourUneTonne(), 4)+" pour "+Journal.texteColore(proposition.getVendeur(), Journal.doubleSur(proposition.getQuantiteEnTonnes(), 2)+" tonnes de "+proposition.getFeve().name())+Journal.texteColore(Color.red, Color.white, " a ete refusee"));
-		//		this.journal.ajouter("--> augmente le prix de 2% --> "+Journal.doubleSur(nouveauPrix, 4));
-		//	}
-		//
-		//	public void notifierVente(PropositionCriee proposition) {
-		//		this.stocksFeves.put(proposition.getFeve(), this.stocksFeves.get(proposition.getFeve())+proposition.getQuantiteEnTonnes());
-		//		this.journal.ajouter("Apprend que sa proposition de "+Journal.doubleSur(proposition.getPrixPourUneTonne(), 4)+" pour "+Journal.texteColore(proposition.getVendeur(), Journal.doubleSur(proposition.getQuantiteEnTonnes(), 2)+" tonnes de "+proposition.getFeve().name())+Journal.texteColore(Color.green, Color.black," a ete acceptee"));
-		//		this.journal.ajouter("--> le stock de feve passe a "+Journal.doubleSur(this.stocksFeves.get(proposition.getFeve()), 4));
-		//	}
-
-}
+		
