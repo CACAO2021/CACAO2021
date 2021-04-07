e7ce75368c35a89b6ef8427176e3535832c37cf8
emelinemaugez <emeline.maugez@imt-atlantique.net>
2021-04-07 09:12:14 +0200

    Changement valeurs des quantité

--- src/abstraction/eq2Producteur2/Producteur2Valeurs.java
+++ src/abstraction/eq2Producteur2/Producteur2Valeurs.java
@@ -10,12 +10,14 @@ public class Producteur2Valeurs {
 	
 	
 	// partie stockage
-
-	protected static double QTT_FEVE_HBE_DEPART = 0;
-	protected static double QTT_FEVE_HE_DEPART = 0;
-	protected static double QTT_FEVE_ME_DEPART = 0;
-	protected static double QTT_FEVE_M_DEPART = 0;
-	protected static double QTT_FEVE_B_DEPART = 0;
+	
+	protected static double QTT_FEVE_TOTALE = 7.2*Math.pow(10,9)/48;
+	
+	protected static double QTT_FEVE_HBE_DEPART = QTT_FEVE_TOTALE*0.03;
+	protected static double QTT_FEVE_HE_DEPART = QTT_FEVE_TOTALE*0.01;
+	protected static double QTT_FEVE_ME_DEPART = QTT_FEVE_TOTALE*0.01;
+	protected static double QTT_FEVE_M_DEPART = QTT_FEVE_TOTALE*0.4833333;
+	protected static double QTT_FEVE_B_DEPART = QTT_FEVE_TOTALE*0.483333;
 	
 	protected static double QTT_POUDRE_DEPART = 0;
 	
@@ -36,19 +38,31 @@ public class Producteur2Valeurs {
 	
 	protected static double DIF_ACCEPTEE_FEVE_HBE = 1.0;
 	protected static double DIF_ACCEPTEE_FEVE_HE = 1.0;
+	protected static double DIF_ACCEPTEE_FEVE_ME = 1.0;
+	protected static double DIF_ACCEPTEE_FEVE_M = 1.0;
+	protected static double DIF_ACCEPTEE_FEVE_B = 1.0;
 	// a finir
 	
 	
 	// partie production
 	protected int ARBRE_DEBUT_HBE = 5;
 	protected int ARBRE_DEBUT_HE = 0;
+	protected int ARBRE_DEBUT_ME = 0;
+	protected int ARBRE_DEBUT_M = 0;
+	protected int ARBRE_DEBUT_B = 0;
 	//a finir
 	
 	protected int ARBRE_TPS_VIE_HBE = 0;
 	protected int ARBRE_TPS_VIE_HE = 0;
+	protected int ARBRE_TPS_VIE_ME=0;
+	protected int ARBRE_TPS_VIE_M=0;
+	protected int ARBRE_TPS_VIE_B=0;
 	
 	protected int PROD_HBE = 0;
 	protected int PROD_HE = 0;
+	protected int PROD_ME = 0;
+	protected int PROD_M = 0;
+	protected int PROD_B = 0;
 	
 	
 	// verification du type de produit
