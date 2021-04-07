7eac1d115dfde7dd59312a3376d3f6f19cdb3245
paulgiraud <paulgiraud@dhcp-125-142.mde.emn.fr>
2021-04-07 08:23:13 +0200

    stock extends notre acteur

--- src/abstraction/eq3Transformateur1/Stock.java
+++ src/abstraction/eq3Transformateur1/Stock.java
@@ -9,11 +9,13 @@ import abstraction.fourni.Variable;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Stock {
+public class Stock extends Transformateur1Acteur {
 	
 	private Transformateur1Acteur eticao;
 	private List<Variable> indicteurs;
 	protected Map<Feve, Double> stocksFeves;
 	
-
+	public Stock() { 
+		super();
+	}
 }
