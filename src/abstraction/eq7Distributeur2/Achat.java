f4c4b3bca3fbf08b1031cab626e4e7a742273e14
Martin Collemare <martin.collemare@imt-atlantique.net>
2021-04-07 12:16:06 +0200

    Achat V2 Martin

--- src/abstraction/eq7Distributeur2/Achat.java
+++ src/abstraction/eq7Distributeur2/Achat.java
@@ -85,7 +85,7 @@ public class Achat extends Distributeur2Acteur implements IAcheteurContratCadre 
 
 
 	public void receptionner(Object produit, double quantite, ExemplaireContratCadre contrat) {
-		
+		stocks.ajouterChocolatDeMarque((ChocolatDeMarque) contrat.getProduit(), contrat.getQuantiteTotale());
 		
 		
 	}
