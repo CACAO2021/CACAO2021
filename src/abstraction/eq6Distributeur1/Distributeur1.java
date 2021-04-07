24a1ebe8a6ecd755f3a9b79c7cfbdbf477eb62bc
giboi <giboi@172.16.122.245>
2021-04-07 08:34:49 +0200

    début d'héritage

--- src/abstraction/eq6Distributeur1/Distributeur1.java
+++ src/abstraction/eq6Distributeur1/Distributeur1.java
@@ -2,7 +2,7 @@ package abstraction.eq6Distributeur1;
 
 import java.util.Map;
 
-public class Distributeur1 extends Distributeur1Acteur {
+public class Distributeur1 extends Vendeur {
 
 	public Distributeur1() {
 		super();
--- src/abstraction/eq6Distributeur1/Vendeur.java
+++ src/abstraction/eq6Distributeur1/Vendeur.java
@@ -1,5 +1,47 @@
 package abstraction.eq6Distributeur1;
 
-public class Vendeur {
+import java.util.List;
+
+import abstraction.eq8Romu.clients.ClientFinal;
+import abstraction.eq8Romu.produits.ChocolatDeMarque;
+import abstraction.fourni.IDistributeurChocolatDeMarque;
+
+public class Vendeur extends Distributeur1Acteur implements IDistributeurChocolatDeMarque{
+
+	@Override
+	public List<ChocolatDeMarque> getCatalogue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double prix(ChocolatDeMarque choco) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double quantiteEnVente(ChocolatDeMarque choco) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double quantiteEnVenteTG(ChocolatDeMarque choco) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void vendre(ClientFinal client, ChocolatDeMarque choco, double quantite, double montant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notificationRayonVide(ChocolatDeMarque choco) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
