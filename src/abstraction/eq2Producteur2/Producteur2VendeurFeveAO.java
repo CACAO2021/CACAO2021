92400fe356dbe91ca46606cdb89d231fae486030
Maxime <Maxime@LAPTOP-SO8TAPIU>
2021-04-07 09:05:16 +0200

    ContratAO

--- src/abstraction/eq2Producteur2/Producteur2VendeurFeveAO.java
+++ src/abstraction/eq2Producteur2/Producteur2VendeurFeveAO.java
@@ -9,37 +9,43 @@ import abstraction.eq8Romu.fevesAO.OffreAchatFeves;
 import abstraction.eq8Romu.fevesAO.PropositionVenteFevesAO;
 
 public class Producteur2VendeurFeveAO extends Producteur2Acteur implements IVendeurFevesAO {
-<<<<<<< HEAD
-	public double prixMinVenteAuKilo = 2.0;
-=======
-	protected LinkedList<ExemplaireContratCadre> mesContratsAO;
 
+	public double prixMinVenteAuKilo = 2.0;
+	protected LinkedList<ExemplaireContratCadre> mesContratsAO;
+	public LinkedList<PropositionVenteFevesAO> mesContratsAORefuses;
+	public LinkedList<PropositionVenteFevesAO> mesContratsAOAcceptes;
+/**	@author Maxime Boillot
+ 
+ */
 	public Producteur2VendeurFeveAO() {
 		super();
 		this.mesContratsAO = new LinkedList<ExemplaireContratCadre>();
 	}
 
->>>>>>> branch 'master' of https://github.com/dim-correia/CACAO2021
-	@Override
+	/**	@author Maxime Boillot
+	 
+	 */
 	public double proposerPrix(OffreAchatFeves oa) {
-	if ((this.pasEnFaillite())) {
-		//Condition à revoir pasEnFaillite à changer ou à écrire?
+	if (stock>=oa.getQuantiteKG()) {
+	//Revoir la condition ci-dessus
 		return prixMinVenteAuKilo;
 	}
 	else {
 		return 0.0;
 	}
 	}
-
-	@Override
+/**	@author Maxime Boillot
+	 
+	 */
 	public void notifierPropositionRefusee(PropositionVenteFevesAO proposition) {
-		// TODO Auto-generated method stub
-		
+		mesContratsAORefuses.add(proposition);
 	}
 
-	@Override
+	/**	@author Maxime Boillot
+	 
+	 */
 	public void notifierVente(PropositionVenteFevesAO proposition) {
-		// TODO Auto-generated method stub
+		mesContratsAOAcceptes.add(proposition);
 		
 	}
 	
