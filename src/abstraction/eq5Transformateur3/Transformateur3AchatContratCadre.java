a8e2abdb0cb24f6467b946834e7cc87c6049e267
manue <manue@LAPTOP-BC6HO7L6>
2021-04-07 08:11:41 +0200

    Changement

--- src/abstraction/eq5Transformateur3/Transformateur3AchatContratCadre.java
+++ src/abstraction/eq5Transformateur3/Transformateur3AchatContratCadre.java
@@ -1,17 +1,24 @@
 package abstraction.eq5Transformateur3;
 
+import java.awt.Color;
 import java.util.List;
 
+import abstraction.eq8Romu.contratsCadres.Echeancier;
 import abstraction.eq8Romu.contratsCadres.ExemplaireContratCadre;
+import abstraction.eq8Romu.contratsCadres.IAcheteurContratCadre;
+import abstraction.fourni.Filiere;
+import abstraction.fourni.IActeur;
+import abstraction.fourni.Journal;
+import abstraction.fourni.Variable;
 
 public class Transformateur3AchatContratCadre implements IAcheteurContratCadre {
 	
-	public Echeancier contrePropositionDeLAcheteur(ExemplaireContratCadre contrat) {
+	public Echeancier contrePropositionDeLAcheteur1(ExemplaireContratCadre contrat) {
 		List<Echeancier> precedentesPropositions = contrat.getEcheanciers();
 		Echeancier dernierEcheancier = contrat.getEcheancier();
 		if () {
 			return null;
-		} elif () {
+		} else if () {
 			return dernierEcheancier;
 		} else {
 			return new Echeancier(stepDebut, nbStep, quantiteParStep);
@@ -21,17 +28,101 @@ public class Transformateur3AchatContratCadre implements IAcheteurContratCadre {
 	public double contrePropositionPrixAcheteur(ExemplaireContratCadre contrat) {
 		List<Double> precedentesPropositions = contrat.getListePrix();
 		Double dernierPrix = contrat.getPrix();
-		if () {
+		if (dernierPrix>) {
 			return -1;
-		} elif(){
+		} else if(dernierPrix<=){
 			return dernierPrix;
 		} else {
-			return ;
+			return (1+%)*dernierPrix;
 		}
 	}
 	
 	public void receptionner(Object produit, double quantite, ExemplaireContratCadre contrat) {
 		
+	}
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initialiser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<String> getNomsFilieresProposees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Filiere getFiliere(String nom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Variable> getIndicateurs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Variable> getParametres() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Journal> getJournaux() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCryptogramme(Integer crypto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notificationFaillite(IActeur acteur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notificationOperationBancaire(double montant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Echeancier contrePropositionDeLAcheteur(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
