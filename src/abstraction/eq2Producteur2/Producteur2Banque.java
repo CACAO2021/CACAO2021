de360d73a798bb68fcf06dd7e1983c8c6dd01ce2
emelinemaugez <emeline.maugez@imt-atlantique.net>
2021-04-07 10:34:07 +0200

    Changement extends

--- src/abstraction/eq2Producteur2/Producteur2Banque.java
+++ src/abstraction/eq2Producteur2/Producteur2Banque.java
@@ -1,6 +1,6 @@
 package abstraction.eq2Producteur2;
 
-public class Producteur2Banque extends Producteur2Prod {
+public class Producteur2Banque extends Producteur2VeudeurFeveCC {
 
 	public Producteur2Banque() {
 		super();
