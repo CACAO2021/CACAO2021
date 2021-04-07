8eb75a0e6572959bd98ba93e02900e8209d5f671
giboi <giboi@172.16.122.245>
2021-04-07 08:26:28 +0200

    renommé les classes

--- src/abstraction/eq6Distributeur1/Acheteur.java
+++ src/abstraction/eq6Distributeur1/Acheteur.java
@@ -1,5 +1,5 @@
 package abstraction.eq6Distributeur1;
 
-public class Vente {
+public class Acheteur {
 
 }
--- src/abstraction/eq6Distributeur1/Vendeur.java
+++ src/abstraction/eq6Distributeur1/Vendeur.java
@@ -1,5 +1,5 @@
 package abstraction.eq6Distributeur1;
 
-public class Achats {
+public class Vendeur {
 
 }
