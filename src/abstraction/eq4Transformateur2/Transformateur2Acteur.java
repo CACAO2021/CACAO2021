665f511e4478643010dacd9b9c61434607782a3a
AntoineC-GIT <antoinecorre.net@gmail.com>
2021-04-14 08:36:33 +0200

    param

--- src/abstraction/eq4Transformateur2/Transformateur2Acteur.java
+++ src/abstraction/eq4Transformateur2/Transformateur2Acteur.java
@@ -72,8 +72,8 @@ public class Transformateur2Acteur extends Transformateur2Valeurs implements IAc
 	
 	public List<Variable> getParametres() {
 		// on choisit les paramètres qui seront pris en compte à l'initialisation de la filière
-		//List<Variable> res=new ArrayList<Variable>();
-		return null; 
+		List<Variable> res=new ArrayList<Variable>();
+		return res; 
 	}
 
 	public List<Journal> getJournaux() {
