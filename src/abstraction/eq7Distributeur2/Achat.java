79b7cecd53a78078486910adcc7d7cf8a1ba56ec
Utilisateur <Utilisateur@172.16.132.193>
2021-04-14 10:19:47 +0200

    22

--- src/abstraction/eq7Distributeur2/Achat.java
+++ src/abstraction/eq7Distributeur2/Achat.java
@@ -28,7 +28,6 @@ public class Achat extends Distributeur2Acteur implements IAcheteurContratCadre 
 	public Color newContratColor = Color.GREEN;
 	public Color newPropositionColor = Color.ORANGE;
 	public Color descriptionColor = Color.YELLOW;
-	public double test2;
 	
 	
 
