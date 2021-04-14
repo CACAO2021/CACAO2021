9aa59825ec1fafc37af2d876d505bbcb2aeb756e
Elsadc7 <81296640+Elsadc7@users.noreply.github.com>
2021-04-14 10:25:31 +0200

    tours nego

--- src/abstraction/eq6Distributeur1/Acheteur.java
+++ src/abstraction/eq6Distributeur1/Acheteur.java
@@ -7,9 +7,7 @@ import abstraction.eq8Romu.produits.ChocolatDeMarque;
 import abstraction.fourni.Journal;
 
 public class Acheteur extends Vendeur implements IAcheteurContratCadre {
-	private double maxQuantite=10000;
-	private double minQuantite=0;
-	private double maxPrix=10000; //Pour les contrats cadres, on regarde les quantités nécéssaires et notre prix max auquels cas on ne réalise pas le contrat.
+	protected int i=0; //Nombre de tours de négociations
 
 	
 	protected Journal JournalAchats= new Journal(this.getNom()+" achats", this);
@@ -19,6 +17,7 @@ public class Acheteur extends Vendeur implements IAcheteurContratCadre {
 	@Override
 	public Echeancier contrePropositionDeLAcheteur(ExemplaireContratCadre contrat) {
 			Echeancier e = contrat.getEcheancier();
+			double maxQuantite= (this.stock.get((ChocolatDeMarque)contrat.getProduit()).getValeur())*1.1; //J'achete 10% de plus que ce que j'ai vendu moins ce qu'il me reste en stock
 			e.set(e.getStepDebut(), e.getQuantite(e.getStepDebut())*2);// on propose d'acheter 2 fois plus si le vendeur n'est pas content. A modifier par la suite
 			return e;
 		
@@ -26,11 +25,11 @@ public class Acheteur extends Vendeur implements IAcheteurContratCadre {
 
 	@Override
 	public double contrePropositionPrixAcheteur(ExemplaireContratCadre contrat) {
-		int i=contrat.getEcheancier().getNbEcheances(); //Nombre d'etapes de négociation déjà passées.
+		i++; //Nombre d'etapes de négociation déjà passées.
 		double maxPrix= this.prix.get((ChocolatDeMarque)contrat.getProduit())*0.75;
 		if (contrat.getTeteGondole()) {
 			maxPrix=0.9*maxPrix;}
-		if (contrat.getPrix()>this.maxPrix) {
+		if (contrat.getPrix()>maxPrix) {
 			return maxPrix*(0.85+i/100);}
 		else {
 			return contrat.getPrix(); 
