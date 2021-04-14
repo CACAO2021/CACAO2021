eb3feee5a0c70b50d35481585c89d7f6239a80c8
Elsadc7 <81296640+Elsadc7@users.noreply.github.com>
2021-04-14 09:45:20 +0200

    contrePropositionDeLAcheteur

--- src/abstraction/eq6Distributeur1/Acheteur.java
+++ src/abstraction/eq6Distributeur1/Acheteur.java
@@ -3,17 +3,21 @@ package abstraction.eq6Distributeur1;
 import abstraction.eq8Romu.contratsCadres.Echeancier;
 import abstraction.eq8Romu.contratsCadres.ExemplaireContratCadre;
 import abstraction.eq8Romu.contratsCadres.IAcheteurContratCadre;
+import abstraction.eq8Romu.produits.ChocolatDeMarque;
 import abstraction.fourni.Journal;
 
 public class Acheteur extends Vendeur implements IAcheteurContratCadre {
+	private double maxQuantite=10000;
+	private double minQuantite=0;
+	private double maxPrix=10000; //Pour les contrats cadres, on regarde les quantités nécéssaires et notre prix max auquels cas on ne réalise pas le contrat.
 
 	
 	protected Journal JournalAchats= new Journal(this.getNom()+" achats", this);
 	
 	
+	
 	@Override
 	public Echeancier contrePropositionDeLAcheteur(ExemplaireContratCadre contrat) {
-		
 			Echeancier e = contrat.getEcheancier();
 			e.set(e.getStepDebut(), e.getQuantite(e.getStepDebut())*2);// on propose d'acheter 2 fois plus si le vendeur n'est pas content. A modifier par la suite
 			return e;
@@ -22,7 +26,14 @@ public class Acheteur extends Vendeur implements IAcheteurContratCadre {
 
 	@Override
 	public double contrePropositionPrixAcheteur(ExemplaireContratCadre contrat) {
-		return contrat.getPrix()*0.95; //On baisse un peu le prix si le vendeur n'est pas content. A modifier par la suite
+		double maxPrix= this.prix.get((ChocolatDeMarque)contrat.getProduit())*0.75;
+		if (contrat.getTeteGondole()) {
+			maxPrix=0.9*maxPrix;}
+		if (contrat.getPrix()>this.maxPrix) {
+			return maxPrix;}
+		else {
+			return contrat.getPrix()*0.95; 
+		}
 	}
 
 	@Override
@@ -31,5 +42,7 @@ public class Acheteur extends Vendeur implements IAcheteurContratCadre {
 		JournalAchats.ajouter("achat de "+quantite+" "+produit.toString()+" a "+contrat.getVendeur().toString()+" pour un prix de "+contrat.getPrix());
 		
 	}
+	
+	
 
 }
