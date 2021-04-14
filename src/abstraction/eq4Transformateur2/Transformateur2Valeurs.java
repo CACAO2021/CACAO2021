79117bf3dd52245a41f473255ead6fd40cb8a031
abigb <abigb@172.16.126.8>
2021-04-14 11:56:42 +0200

    k

--- src/abstraction/eq4Transformateur2/Transformateur2Valeurs.java
+++ src/abstraction/eq4Transformateur2/Transformateur2Valeurs.java
@@ -31,18 +31,17 @@ public class Transformateur2Valeurs {
 	protected double mini_stock_bas;
 	protected double mini_stock_moyen;
 	
-<<<<<<< HEAD
+
 	//Achat 
 	
 	protected static double cout_max_feve_basse ; 
 	protected static double cout_max_feve_moyenne;
-=======
 	protected static double cout_stockage_unite_feve;
 	protected static double cout_stockage_unite_choco;
 	
 	protected static double cout_fixe_entrepot_feve;
 	protected static double cout_fixe_entrepot_choco;
->>>>>>> branch 'master' of https://github.com/Romaric-Moyeuvre/CACAO2021
+
 	
 	//Production
 	
