eec6699a911d81261c1d848ec6bcd2ff2008d1f4
Dimitri CORREIA <correiadimitri@sfr.fr>
2021-04-14 09:27:20 +0200

    testeur

--- src/abstraction/eq2Producteur2/Producteur2TestDesVentes.java
+++ src/abstraction/eq2Producteur2/Producteur2TestDesVentes.java
@@ -1,21 +1,13 @@
 package abstraction.eq2Producteur2;
 
-public class Producteur2TestDesVentes {
-	package abstraction.eq8Romu.fevesAO;
+import abstraction.eq8Romu.produits.Feve;
+import abstraction.fourni.Filiere;
+import abstraction.fourni.IActeur;
 
+public class Producteur2TestDesVentes  extends Filiere {
 
-
-	import abstraction.eq8Romu.produits.Feve;
-	import abstraction.fourni.Filiere;
-	import abstraction.fourni.IActeur;
-
-	public class FiliereTestAOFeves extends Filiere {
-
-		public FiliereTestAOFeves() {
+		public Producteur2TestDesVentes() {
 			super();
-			this.ajouterActeur(new ExempleVendeurFevesAO(Feve.FEVE_BASSE, 1.0, 20.0, 0.65, 1.65));
-			this.ajouterActeur(new ExempleVendeurFevesAO(Feve.FEVE_BASSE, 1.0, 20.0, 0.75, 1.35));
-			this.ajouterActeur(new ExempleVendeurFevesAO(Feve.FEVE_MOYENNE, 0.5, 10.0, 0.95, 1.95));
 			this.ajouterActeur(new ExempleAcheteurFevesAO(Feve.FEVE_BASSE, 0.85, 500, 4000));
 			this.ajouterActeur(new ExempleAcheteurFevesAO(Feve.FEVE_BASSE, 0.8, 1000, 2000));
 			this.ajouterActeur(new ExempleAcheteurFevesAO(Feve.FEVE_MOYENNE, 1.1, 250, 1500));
