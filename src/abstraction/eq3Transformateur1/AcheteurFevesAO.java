aae5b6caaf950f3a7bff234443ee5e379858215e
cheva <cheva@172.16.127.184>
2021-04-14 10:13:08 +0200

    achat

--- src/abstraction/eq3Transformateur1/AcheteurFevesAO.java
+++ src/abstraction/eq3Transformateur1/AcheteurFevesAO.java
@@ -8,6 +8,7 @@ import abstraction.eq8Romu.fevesAO.OffreAchatFeves;
 import abstraction.eq8Romu.fevesAO.PropositionVenteFevesAO;
 import abstraction.eq8Romu.fevesAO.SuperviseurVentesFevesAO;
 import abstraction.eq8Romu.produits.Feve;
+import abstraction.fourni.Filiere;
 import abstraction.fourni.Journal;
 import abstraction.fourni.Variable;
 
@@ -79,10 +80,14 @@ public class AcheteurFevesAO extends AcheteurFevesContratCadre implements IAchet
 	}
 	
 	public OffreAchatFeves getOffreAchat() {
-		double quantite = (Double) null ;
+		double quantite = null ;
 		OffreAchatFeves result = new OffreAchatFeves(this, feve, quantite );
-		 
-		if () { // une chance sur 2
+		if (qMin )
+		if (qMax<) {
+			quantite = qMax ;
+		} else {
+			quantite  = ;
+		
 			this.journalAcheteur.ajouter("Offre d'achat = "+result);
 			return result;
 		} else {
@@ -97,7 +102,7 @@ public class AcheteurFevesAO extends AcheteurFevesContratCadre implements IAchet
 	}
 
 	public PropositionVenteFevesAO choisirPropositionVenteAOFeves(List<PropositionVenteFevesAO> propositions) {
-		double p = 0;
+		int p = 0;
 		double prixmin = propositions.get(0).getPrixKG();
 		if (propositions.size()>0) {
 			for (int i=0; i<propositions.size(); i++) {
@@ -106,11 +111,15 @@ public class AcheteurFevesAO extends AcheteurFevesContratCadre implements IAchet
 					prixmin=propositions.get(i).getPrixKG();
 				}
 			}
-			if (prixmin<this.prixMax) {
-				return propositions.get(p);
+			if (prixmin<this.prixMax) { 
+				if (this.getSolde()>prixmin) {
+					return propositions.get(p);
+			} else {
+				return null ;
 			}
 		}
-		return null;
+		return null ;
+		}
 	}
 
 	public void notifierVente(PropositionVenteFevesAO proposition) {
@@ -119,30 +128,5 @@ public class AcheteurFevesAO extends AcheteurFevesContratCadre implements IAchet
 		this.getStock().setStockFeve(feve, quantite, prix);
 		this.journalAcheteur.ajouter("--> le stock de " + proposition.getFeve().toString() + "passe a "+Journal.doubleSur(this.getStock().getStockFeves(proposition.getFeve()), 4));
 	}
-
-	//	public double proposerAchat(LotCacaoCriee lot) {
-	//		double prix = this.prixCourant.get(lot.getFeve())*0.95; // on tente de payer un peu moins
-	//		double solde = Filiere.LA_FILIERE.getBanque().getSolde(this, cryptogramme);
-	//		if (solde>lot.getQuantiteEnTonnes()*prix) {
-	//			this.journal.ajouter("Propose "+Journal.doubleSur(prix,  4)+" pour "+Journal.texteColore(lot.getVendeur(), Journal.doubleSur(lot.getQuantiteEnTonnes(), 2)+" tonnes de "+lot.getFeve().name()));
-	//			return prix;
-	//		} else {
-	//			this.journal.ajouter("Ne souhaite pas acheter un lot de "+Journal.texteColore(lot.getVendeur(), Journal.doubleSur(lot.getQuantiteEnTonnes(), 2)+" tonnes de "+lot.getFeve().name()));
-	//			return 0.0;
-	//		}
-	//	}
-	//
-	//	public void notifierPropositionRefusee(PropositionCriee proposition) {
-	//		double nouveauPrix = Math.max(0.01,  this.prixCourant.get(proposition.getLot().getFeve())*1.02);
-	//		this.prixCourant.put(proposition.getLot().getFeve(), nouveauPrix);
-	//		this.journal.ajouter("Apprend que sa proposition de "+Journal.doubleSur(proposition.getPrixPourUneTonne(), 4)+" pour "+Journal.texteColore(proposition.getVendeur(), Journal.doubleSur(proposition.getQuantiteEnTonnes(), 2)+" tonnes de "+proposition.getFeve().name())+Journal.texteColore(Color.red, Color.white, " a ete refusee"));
-	//		this.journal.ajouter("--> augmente le prix de 2% --> "+Journal.doubleSur(nouveauPrix, 4));
-	//	}
-	//
-	//	public void notifierVente(PropositionCriee proposition) {
-	//		this.stocksFeves.put(proposition.getFeve(), this.stocksFeves.get(proposition.getFeve())+proposition.getQuantiteEnTonnes());
-	//		this.journal.ajouter("Apprend que sa proposition de "+Journal.doubleSur(proposition.getPrixPourUneTonne(), 4)+" pour "+Journal.texteColore(proposition.getVendeur(), Journal.doubleSur(proposition.getQuantiteEnTonnes(), 2)+" tonnes de "+proposition.getFeve().name())+Journal.texteColore(Color.green, Color.black," a ete acceptee"));
-	//		this.journal.ajouter("--> le stock de feve passe a "+Journal.doubleSur(this.stocksFeves.get(proposition.getFeve()), 4));
-	//	}
 }
 
