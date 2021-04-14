f305a486c733426d34100aa474378c7b2c9f6c3e
elo <elo@dhcp-127-61.mde.emn.fr>
2021-04-14 08:47:02 +0200

    Coût transfo poudre

--- src/abstraction/eq2Producteur2/Producteur2et1ValeursEnCommun.java
+++ src/abstraction/eq2Producteur2/Producteur2et1ValeursEnCommun.java
@@ -33,7 +33,7 @@ public class Producteur2et1ValeursEnCommun {
 	protected int qteParStepME = 1;
 	
 	protected double prixHEparKilo = 1; // coût de transformation d'un kilo de fèves en poudre
-	protected double prixMparKilo = 1;
+	protected double prixMparKilo = 1; // un seul coût: entretien des machines
 	protected double prixMEparKilo = 1;
 
 }
