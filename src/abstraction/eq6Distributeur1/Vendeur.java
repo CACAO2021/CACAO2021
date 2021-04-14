871f96cfb98e607c757e5015c2a63ea43b09abe4
ThomasYamini <81296363+ThomasYamini@users.noreply.github.com>
2021-04-14 10:52:09 +0200

    update

--- src/abstraction/eq6Distributeur1/Vendeur.java
+++ src/abstraction/eq6Distributeur1/Vendeur.java
@@ -7,7 +7,9 @@ import java.util.Set;
 
 import abstraction.eq8Romu.clients.ClientFinal;
 import abstraction.eq8Romu.produits.ChocolatDeMarque;
+import abstraction.fourni.Banque;
 import abstraction.fourni.IDistributeurChocolatDeMarque;
+import abstraction.fourni.Variable;
 
 public class Vendeur extends Stocks implements IDistributeurChocolatDeMarque{
 	
@@ -30,7 +32,7 @@ public class Vendeur extends Stocks implements IDistributeurChocolatDeMarque{
 	@Override
 	public double quantiteEnVente(ChocolatDeMarque choco) {
 		if (choco!=null) {
-			return this.stock.get(choco).getValeur() + quantiteEnVenteTG(choco);
+			return this.stock.get(choco).getValeur();
 		}
 		else {
 			return 0;
@@ -44,19 +46,27 @@ public class Vendeur extends Stocks implements IDistributeurChocolatDeMarque{
 		}
 		else {
 			return 0;
-		}
+		}  
 	}
 
 	@Override
 	public void vendre(ClientFinal client, ChocolatDeMarque choco, double quantite, double montant) {
-		// TODO Auto-generated method stub
-		
+		List<String> historique = new ArrayList<String>();
+		vendre(client, choco, quantite, montant, historique);
+	}
+	
+	public void vendre(ClientFinal client, ChocolatDeMarque choco, double quantite, double montant, List<String> historique) {
+		if(choco!=null && quantite>0 && quantite<this.quantiteEnVente(choco)) {
+			this.ajouterStock(choco, -1*quantite, false);
+			historique.add(choco.getMarque()+" : "+quantite);
+		}
 	}
 
 	@Override
 	public void notificationRayonVide(ChocolatDeMarque choco) {
-		// TODO Auto-generated method stub
-		
-	}
-
+		if (quantiteEnVente(choco)==0) {
+			System.out.println("Plus de : "+ choco.getMarque()+" en rayon");
+		}
+	}	
 }
+
