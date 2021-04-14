11dd2af7768bfacdfc3674ca4bee4fcb20fa09a2
AntoineC-GIT <antoinecorre.net@gmail.com>
2021-04-14 08:18:16 +0200

    création variables stocks

--- src/abstraction/eq4Transformateur2/Transformateur2Stock.java
+++ src/abstraction/eq4Transformateur2/Transformateur2Stock.java
@@ -1,4 +1,6 @@
 package abstraction.eq4Transformateur2;
+import java.util.HashMap;
+
 import abstraction.eq8Romu.produits.Chocolat;
 import abstraction.eq8Romu.produits.Feve;
 
@@ -9,6 +11,14 @@ public class Transformateur2Stock extends Transformateur2Production {
 	
 	public Transformateur2Stock() {
 		super();
+		stock_feve = new HashMap<Feve, Double>();
+		stock_chocolat = new HashMap<Chocolat, Double>();
+		stock_feve.put(Feve.FEVE_BASSE, 0.0);
+		stock_feve.put(Feve.FEVE_MOYENNE, 0.0);
+		stock_chocolat.put(Chocolat.CONFISERIE_BASSE, 0.0);
+		stock_chocolat.put(Chocolat.CONFISERIE_MOYENNE, 0.0);
+		stock_chocolat.put(Chocolat.TABLETTE_BASSE, 0.0);
+		stock_chocolat.put(Chocolat.TABLETTE_MOYENNE, 0.0);
 	}
 	
 	public void add_stock(Object o, double quantite) {
