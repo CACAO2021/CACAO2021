bbd57cdcfbbd8a5e7acb98c17995036a2f8a5f96
jonathanaderkaoui <jonathanaderkaoui@air-de-jonathan-001>
2021-04-21 09:06:47 +0200

    J'ai remis les chocolats de marque

--- src/abstraction/eq3Transformateur1/Transformateur1FabricantChocolatDeMarque.java
+++ src/abstraction/eq3Transformateur1/Transformateur1FabricantChocolatDeMarque.java
@@ -1,6 +1,7 @@
 package abstraction.eq3Transformateur1;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -19,6 +20,19 @@ public class Transformateur1FabricantChocolatDeMarque extends Transformateur1Act
 	
 	public Transformateur1FabricantChocolatDeMarque() {
 		super();
+		this.marque="Eticao";
+		this.chocolats=new LinkedList<ChocolatDeMarque> ();
+		chocolats.add(new ChocolatDeMarque(Chocolat.CONFISERIE_HAUTE_BIO_EQUITABLE,marque));
+		chocolats.add(new ChocolatDeMarque(Chocolat.CONFISERIE_HAUTE_EQUITABLE,marque));
+		chocolats.add(new ChocolatDeMarque(Chocolat.CONFISERIE_MOYENNE_EQUITABLE,marque));
+		chocolats.add(new ChocolatDeMarque(Chocolat.POUDRE_HAUTE_BIO_EQUITABLE,marque));
+		chocolats.add(new ChocolatDeMarque(Chocolat.POUDRE_HAUTE_EQUITABLE,marque));
+		chocolats.add(new ChocolatDeMarque(Chocolat.POUDRE_MOYENNE_EQUITABLE,marque));
+		chocolats.add(new ChocolatDeMarque(Chocolat.TABLETTE_HAUTE_BIO_EQUITABLE,marque));
+		chocolats.add(new ChocolatDeMarque(Chocolat.TABLETTE_HAUTE_EQUITABLE,marque));
+		chocolats.add(new ChocolatDeMarque(Chocolat.TABLETTE_MOYENNE_EQUITABLE,marque));
+		
+		
 
 	}
 		
