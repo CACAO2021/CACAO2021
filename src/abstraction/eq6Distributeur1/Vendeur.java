6f90fa8cda3e4f33798ce21cb50562c315e2e8cd
thoma <thoma@172.16.121.61>
2021-04-21 10:22:11 +0200

    up

--- src/abstraction/eq6Distributeur1/Vendeur.java
+++ src/abstraction/eq6Distributeur1/Vendeur.java
@@ -18,7 +18,7 @@ public class Vendeur extends Stocks implements IDistributeurChocolatDeMarque{
 	protected Map<ChocolatDeMarque,Double> historique;
 	protected int quantitevendue; // quantite vendue en une période
 	protected int q;//Quantité à définir à partir duquel les ventes sont convenables
-	
+	 
 	public int getQuantiteVendue() {
 		return this.quantitevendue;
 	}
