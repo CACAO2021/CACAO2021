d3a80dc564a13d172bc6f3f4d5c15ed587be548a
Dimitri CORREIA <correiadimitri@sfr.fr>
2021-05-05 11:51:49 +0200

    remise en forme fichier

--- test/abstraction/FiliereParDefaultTest.java
+++ test/abstraction/FiliereParDefaultTest.java
@@ -10,7 +10,6 @@ public class FiliereParDefaultTest {
 	@Test
 	public void testNext() {
 		Filiere.LA_FILIERE = null;
-		System.out.println(Filiere.LA_FILIERE.getMois());
 		Filiere.LA_FILIERE = new FiliereParDefaut();
 		Filiere.LA_FILIERE.initialiser();
 		for (int i=0; i<300; i++)
