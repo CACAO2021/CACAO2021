2719b377c339658b4450571191dd927b092ad961
Manuelo <manuelo.douya-nkolla@imt-atlantique.net>
2021-05-05 09:01:53 +0200

    bug corrigé

--- src/abstraction/eq5Transformateur3/Transformateur3Acteur.java
+++ src/abstraction/eq5Transformateur3/Transformateur3Acteur.java
@@ -111,10 +111,11 @@ public abstract class Transformateur3Acteur implements IActeur {
 		if(feve.getValeur()<this.stock_min_feves.getValeur()) {
 			IVendeurContratCadre vendeur = null;
 			List<IVendeurContratCadre> vendeurs = SupCCadre1.getVendeurs(Feve.FEVE_MOYENNE);
-
-			vendeur=vendeurs.get((int)( Math.random()*vendeurs.size())); //prend un vendeur aléatoirement
-			ExemplaireContratCadre contratCadre = SupCCadre1.demande((IAcheteurContratCadre)this, vendeur, Feve.FEVE_MOYENNE, new Echeancier(Filiere.LA_FILIERE.getEtape()+1, 10, SuperviseurVentesContratCadre.QUANTITE_MIN_ECHEANCIER/10), cryptogramme, false); 
-			this.JournalAchatContratCadre.ajouter(contratCadre.toString());
+			if(vendeurs.size()>0) {
+				vendeur=vendeurs.get((int)( Math.random()*vendeurs.size())); //prend un vendeur aléatoirement
+				ExemplaireContratCadre contratCadre = SupCCadre1.demande((IAcheteurContratCadre)this, vendeur, Feve.FEVE_MOYENNE, new Echeancier(Filiere.LA_FILIERE.getEtape()+1, 10, SuperviseurVentesContratCadre.QUANTITE_MIN_ECHEANCIER/10), cryptogramme, false); 
+				this.JournalAchatContratCadre.ajouter(contratCadre.toString());
+			}
 		}
 
 		
@@ -122,11 +123,12 @@ public abstract class Transformateur3Acteur implements IActeur {
 		feve=this.getFeves().get(Feve.FEVE_HAUTE_BIO_EQUITABLE);
 		if(feve.getValeur()<this.stock_min_feves.getValeur()) {
 			IVendeurContratCadre vendeur = null;
-
 			List<IVendeurContratCadre> vendeurs = SupCCadre1.getVendeurs(Feve.FEVE_HAUTE_BIO_EQUITABLE);
-			vendeur=vendeurs.get((int)( Math.random()*vendeurs.size())); //prend un vendeur aléatoirement
-			ExemplaireContratCadre contratCadre = SupCCadre1.demande((IAcheteurContratCadre)this, vendeur, Feve.FEVE_HAUTE_BIO_EQUITABLE, new Echeancier(Filiere.LA_FILIERE.getEtape()+1, 10, SuperviseurVentesContratCadre.QUANTITE_MIN_ECHEANCIER/10), cryptogramme, false);
-			this.JournalAchatContratCadre.ajouter(contratCadre.toString());
+			if(vendeurs.size()>0) {
+				vendeur=vendeurs.get((int)( Math.random()*vendeurs.size())); //prend un vendeur aléatoirement
+				ExemplaireContratCadre contratCadre = SupCCadre1.demande((IAcheteurContratCadre)this, vendeur, Feve.FEVE_HAUTE_BIO_EQUITABLE, new Echeancier(Filiere.LA_FILIERE.getEtape()+1, 10, SuperviseurVentesContratCadre.QUANTITE_MIN_ECHEANCIER/10), cryptogramme, false);
+				this.JournalAchatContratCadre.ajouter(contratCadre.toString());
+			}
 		}
 
 	
