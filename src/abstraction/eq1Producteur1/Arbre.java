de0a01d099d5b42041a072ba7ec35129e7cb0731
Alb1x <alban.tavolacci@imt-atlantique.net>
2021-05-05 11:42:59 +0200

    Ajout arbre V2
    Change-Id: Ib4bd763417233480c5615c504a6f333bbe1de8c8

--- src/abstraction/eq1Producteur1/Arbre.java
+++ src/abstraction/eq1Producteur1/Arbre.java
@@ -2,16 +2,18 @@ package abstraction.eq1Producteur1;
 
 import abstraction.eq8Romu.produits.Feve;
 
+/**
+ * 
+ * @author lebra
+ *
+ */
+
 public class Arbre {
 	
 	private double age;
 	private double rendement;
 	private double nombre_arbre;
-	private Feve feve;
 	
-	public Arbre() {
-		
-	}
 	/**
 	 * @return the nombre_arbre
 	 */
@@ -49,15 +51,34 @@ public class Arbre {
 		this.rendement = rendement;
 	}
 	
+	/**
+	 * 
+	 * l'arbre ne produit rien avant 3 ans
+	 * commence à produire à 3 ans et produit linéarement jusqu'à 6ans
+	 * puis production constante jusqu'à la fin de sa vie
+	 * rendement annuel de 6kg par arbre
+	 */
+	
 	public double rendement() {
-		return this.rendement;
+		if (this.getAge() < 3*24) {
+			return 0;
+		}
+		else if (this.getAge() < 6*24){
+			return ((this.getAge()*6)/(3*23*24)-6/24);
+		}
+		else {
+			return 6/24;
+		}
 	}
 	
-	public Arbre(double age, double nombre_arbre, Feve feve) {
+	public Arbre(double age, double nombre_arbre) {
 		this.age = age;
 		this.nombre_arbre = nombre_arbre;
-		this.feve = feve;
 		this.rendement = this.rendement();
+	}
+	
+	public void augmenter_age() {
+		this.setAge(this.getAge()+1);
 	}
 
 }
