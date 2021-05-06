efa949342c1550989dc6b365207c21b17ff2c571
AntoineC-GIT <antoinecorre.net@gmail.com>
2021-05-06 10:32:56 +0200

    up dinguerie

--- src/abstraction/eq4Transformateur2/Transformateur2AchatAO.java
+++ src/abstraction/eq4Transformateur2/Transformateur2AchatAO.java
@@ -22,10 +22,12 @@ public class Transformateur2AchatAO extends Transformateur2AchatCC implements IA
 	@Override
 	public OffreAchatFeves getOffreAchat() {
 		if (get_stock(Feve.FEVE_BASSE) < quantite_totale_demandee_basse / nombre_step_total_basse + mini_stock_feve_basse) {
-			return new OffreAchatFeves(this, Feve.FEVE_BASSE,quantite_totale_demandee_basse + mini_stock_feve_basse - get_stock(Feve.FEVE_BASSE));
+			//return new OffreAchatFeves(this, Feve.FEVE_BASSE,quantite_totale_demandee_basse + mini_stock_feve_basse - get_stock(Feve.FEVE_BASSE));
+			return new OffreAchatFeves(this, Feve.FEVE_BASSE,mini_stock_feve_basse - get_stock(Feve.FEVE_BASSE));
 		}
 		if (get_stock(Feve.FEVE_MOYENNE) < quantite_totale_demandee_moyenne / nombre_step_total_moyenne + mini_stock_feve_moyenne) {
-			return new OffreAchatFeves(this, Feve.FEVE_MOYENNE, quantite_totale_demandee_moyenne + mini_stock_feve_moyenne - get_stock(Feve.FEVE_MOYENNE));
+			//return new OffreAchatFeves(this, Feve.FEVE_MOYENNE, quantite_totale_demandee_moyenne + mini_stock_feve_moyenne - get_stock(Feve.FEVE_MOYENNE));
+			return new OffreAchatFeves(this, Feve.FEVE_MOYENNE,mini_stock_feve_moyenne - get_stock(Feve.FEVE_MOYENNE));
 		}
 		else {
 			return null;
--- src/abstraction/eq4Transformateur2/Transformateur2Valeurs.java
+++ src/abstraction/eq4Transformateur2/Transformateur2Valeurs.java
@@ -24,11 +24,13 @@ public class Transformateur2Valeurs {
 		var_stock_confiserie_moyenne = new Variable("stock conifserie moyenne", (IActeur) this, quantite_init_confiserie_moyenne);
 	}
 	
+	//Echéancier
+	
 	protected LinkedList<Double> echeancier_basse;
 	protected LinkedList<Double> echeancier_moyenne ;
 	protected LinkedList<Double> echeancier_total ; 
 	
-	
+
 	
 	protected static double Prix_max_achat;
 	
@@ -40,17 +42,19 @@ public class Transformateur2Valeurs {
 	
 	protected HashMap<Feve,Double> stock_feve;
 	protected HashMap<Chocolat,Double> stock_chocolat;
-	protected double mini_stock_tablette_basse = 100000000.0;
-	protected double mini_stock_tablette_moyenne = 100000000.0;
-	protected double mini_stock_confiserie_basse = 100000000.0;
-	protected double mini_stock_confiserie_moyenne = 100000000.0;
-	protected double mini_stock_feve_basse = 100000000.0;
-	protected double mini_stock_feve_moyenne = 100000000.0;
+	protected double mini_stock_tablette_basse = 15000000.0;
+	protected double mini_stock_tablette_moyenne = 15000000.0;
+	protected double mini_stock_confiserie_basse = 15000000.0;
+	protected double mini_stock_confiserie_moyenne = 15000000.0;
+	protected double mini_stock_feve_basse = 10000000.0;
+	protected double mini_stock_feve_moyenne = 10000000.0;
 	
 	protected double quantite_totale_demandee_basse = 0;
 	protected double quantite_totale_demandee_moyenne = 0;
 	protected double nombre_step_total_basse = 0;
 	protected double nombre_step_total_moyenne = 0;
+	
+	//Coûts fixes
 	
 	protected static double cout_stockage_unite_feve = 0.006;
 	protected static double cout_stockage_unite_choco = 0.006 ;
@@ -76,22 +80,16 @@ public class Transformateur2Valeurs {
 	protected double prix_min_confiserie_basse = cout_transformation + cout_stockage_unite_choco+ cout_stockage_unite_feve + cout_max_feve_basse; 
 	protected double prix_min_confiserie_moyenne = cout_transformation + cout_stockage_unite_choco+ cout_stockage_unite_feve + cout_max_feve_moyenne; 
 
-	/*Variables
+	//Variables
 	
-	protected Variable var_stock_feve_basse = new Variable("STOCK_FEVE_BASSE", (IActeur) this, 0, 1000000, stock_feve.get(Feve.FEVE_BASSE));
-	protected Variable var_stock_feve_moyenne = new Variable("STOCK_FEVE_MOYENNE", (IActeur) this, 0, 1000000, stock_feve.get(Feve.FEVE_MOYENNE));
-	protected Variable var_stock_tablette_basse = new Variable("STOCK_TABLETTE_BASSE", (IActeur) this, 0, 1000000, stock_chocolat.get(Chocolat.TABLETTE_BASSE));
-	protected Variable var_stock_tablette_moyenne = new Variable("STOCK_TABLETTE_MOYENNE", (IActeur) this, 0, 1000000, stock_chocolat.get(Chocolat.TABLETTE_MOYENNE));
-	protected Variable var_stock_confiserie_basse = new Variable("STOCK_CONFISERIE_BASSE", (IActeur) this, 0, 1000000, stock_chocolat.get(Chocolat.CONFISERIE_BASSE));
-	protected Variable var_stock_confiserie_moyenne = new Variable("STOCK_CONFISERIE_MOYENNE", (IActeur) this, 0, 1000000, stock_chocolat.get(Chocolat.CONFISERIE_MOYENNE));
-*/
-
 	protected Variable var_stock_feve_basse;
 	protected Variable var_stock_feve_moyenne;
 	protected Variable var_stock_tablette_basse;
 	protected Variable var_stock_tablette_moyenne;
 	protected Variable var_stock_confiserie_basse;
 	protected Variable var_stock_confiserie_moyenne;
+	
+	//Quantité intiale dans les entrepôts
 	
 	protected static double quantite_init_feve_basse = (1627200000.0+4197600000.0)/48.0;
 	protected static double quantite_init_feve_moyenne = (1627200000.0+4197600000.0)/48.0;
