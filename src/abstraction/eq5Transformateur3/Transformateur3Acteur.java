19c0a6b4d1dcb60b739de2310607d61746676fb3
Manuelo <manuelo.douya-nkolla@imt-atlantique.net>
2021-05-19 18:52:22 +0200

    push

--- src/abstraction/eq5Transformateur3/Transformateur3Acteur.java
+++ src/abstraction/eq5Transformateur3/Transformateur3Acteur.java
@@ -60,7 +60,6 @@ public abstract class Transformateur3Acteur implements IActeur {
 		this.prix_tablette_equi = new Variable("Prix tablette équitable", this, 2);
 		this.prix_confiserie = new Variable("Prix confiserie", this, 2);
 		
-		
 	}
 
 	public String getNom() {
