d0f6a3a2ae9810c115e0e202069611eeeba98831
Alb1x <alban.tavolacci@imt-atlantique.net>
2021-05-26 13:15:31 +0200

    Résolution bug

--- src/abstraction/eq1Producteur1/VendeurContratCadre1.java
+++ src/abstraction/eq1Producteur1/VendeurContratCadre1.java
@@ -130,7 +130,7 @@ public abstract class VendeurContratCadre1 extends VendeurFevesAO implements IVe
 	 */
 	public Echeancier contrePropositionDuVendeur(ExemplaireContratCadre contrat) {
 		produit=contrat.getProduit();
-		if ((contrat.getProduit() instanceof Feve) && ((((Feve)produit) == Feve.FEVE_MOYENNE)) ) {
+		if ((produit instanceof Feve) && ((((Feve)produit) == Feve.FEVE_MOYENNE)) ) {
 			if (contrat.getEcheancier().getQuantiteTotale()/contrat.getEcheancier().getNbEcheances() >=  0.25*this.getStocks().get(contrat.getProduit()).getQuantite() || contrat.getEcheancier().getQuantiteTotale()/contrat.getEcheancier().getNbEcheances() <=  0.05*this.getStocks().get(contrat.getProduit()).getQuantite()) {
 				double nvlleqte = 0.15*this.getStocks().get(contrat.getProduit()).getQuantite();
 				Echeancier e = new Echeancier(contrat.getEcheancier().getStepDebut(), contrat.getEcheancier().getStepFin(), ((double)(nvlleqte/(contrat.getEcheancier().getNbEcheances()))));
@@ -167,8 +167,8 @@ public abstract class VendeurContratCadre1 extends VendeurFevesAO implements IVe
 		} else if (contrat.getProduit() instanceof ChocolatDeMarque && ((((ChocolatDeMarque)produit).getChocolat() == Chocolat.POUDRE_MOYENNE_EQUITABLE))) {
 			double duree = contrat.getEcheancier().getStepFin()-contrat.getEcheancier().getStepDebut();
 			if (duree > 8 && contrat.getEcheancier().getQuantiteTotale()/contrat.getEcheancier().getNbEcheances() > EQUI_QTT_MINI) {
-				if (contrat.getEcheancier().getQuantiteTotale()/contrat.getEcheancier().getNbEcheances() >=  0.30*this.getStocks().get(contrat.getProduit()).getQuantite() || contrat.getEcheancier().getQuantiteTotale()/contrat.getEcheancier().getNbEcheances() <=  0.10 *this.getStocks().get(contrat.getProduit()).getQuantite()) {
-					double nvlleqte = 0.2*this.getStocks().get(contrat.getProduit()).getQuantite();
+				if (contrat.getEcheancier().getQuantiteTotale()/contrat.getEcheancier().getNbEcheances() >=  0.30*this.getStocks().get(((ChocolatDeMarque)produit).getChocolat()).getQuantite() || contrat.getEcheancier().getQuantiteTotale()/contrat.getEcheancier().getNbEcheances() <=  0.10 *this.getStocks().get(((ChocolatDeMarque)produit).getChocolat()).getQuantite()) {
+					double nvlleqte = 0.2*this.getStocks().get(((ChocolatDeMarque)produit).getChocolat()).getQuantite();
 					Echeancier e = new Echeancier(contrat.getEcheancier().getStepDebut(), contrat.getEcheancier().getStepFin(), ((double)(nvlleqte/(contrat.getEcheancier().getNbEcheances()))));
 					return e;
 				} else {
@@ -181,8 +181,8 @@ public abstract class VendeurContratCadre1 extends VendeurFevesAO implements IVe
 			}
 			
 		} else if (contrat.getProduit() instanceof Chocolat && ((((ChocolatDeMarque)produit).getChocolat() == Chocolat.POUDRE_MOYENNE))) {
-			if (contrat.getEcheancier().getQuantiteTotale()/contrat.getEcheancier().getNbEcheances() <=  0.60*this.getStocks().get(contrat.getProduit()).getQuantite()/contrat.getEcheancier().getNbEcheances() ) {
-				double nvlleqte = 0.8*this.getStocks().get(contrat.getProduit()).getQuantite();
+			if (contrat.getEcheancier().getQuantiteTotale()/contrat.getEcheancier().getNbEcheances() <=  0.60*this.getStocks().get(((ChocolatDeMarque)produit).getChocolat()).getQuantite()/contrat.getEcheancier().getNbEcheances() ) {
+				double nvlleqte = 0.8*this.getStocks().get(((ChocolatDeMarque)produit).getChocolat()).getQuantite();
 				Echeancier e = new Echeancier(contrat.getEcheancier().getStepDebut(), contrat.getEcheancier().getStepFin(), ((double)(nvlleqte/(contrat.getEcheancier().getNbEcheances()))));
 				return e;
 			} else {
